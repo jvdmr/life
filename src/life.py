e30fc4f78e2b67a4485550ed5616a80d63c10edb
Joris Vandermeersch <joris@vdmr.be>
2020-04-13 12:52:35 +0200

    Optimizations: - integrate display loop into calculation loop instead of doing two loops every step - cache neighbour coordinates instead of recalculating them every step - reuse existing world arrays instead of creating a new array every step

--- src/life.py
+++ src/life.py
@@ -5,6 +5,7 @@ import time
 
 
 REACH = 1
+NEIGHBOURHOODS = []
 
 
 def die(world, x, y):
@@ -19,63 +20,55 @@ def alive(world, x, y):
     return world[x][y]
 
 
-def count_world(world):
-    n = 0
-    max_x = len(world)
-    max_y = len(world[0])
-    for x in range(max_x):
-        for y in range(max_y):
-            if alive(world, x, y):
-                n += 1
-    return n
+def find_neighbours(x, y):
+    neighbourhood = NEIGHBOURHOODS[x][y]
+    if not neighbourhood:
+        neighbourhood = [
+            [nx, ny]
+            for nx in [x + i for i in range(-REACH, REACH + 1)]
+            for ny in [y + i for i in range(-REACH, REACH + 1)]
+            if not (x == nx and y == ny)
+        ]
+        NEIGHBOURHOODS[x][y] = neighbourhood
+    return neighbourhood
 
 
 def count_neighbours(world, x, y):
     n = 0
     max_x = len(world)
     max_y = len(world[0])
-    for nx in [x + i for i in range(-REACH, REACH + 1)]:
+    for nx, ny in find_neighbours(x, y):
         # if not 0 <= nx < max_x:
         #     continue
-        for ny in [y + i for i in range(-REACH, REACH + 1)]:
-            # if not 0 <= ny < max_y:
-            #     continue
-            if x == nx and y == ny:
-                continue
-            if alive(world, nx % max_x, ny % max_y):
-                n += 1
+        # if not 0 <= ny < max_y:
+        #     continue
+        if alive(world, nx % max_x, ny % max_y):
+            n += 1
     return n
 
 
 def check_livelihood(world, x, y):
     neighbours = count_neighbours(world, x, y)
     if alive(world, x, y):
-        return 2 <= neighbours <= 3
+        return (REACH + 1) <= neighbours <= (REACH * 3)
     else:
-        return neighbours == 3
+        return neighbours == (1 + (REACH * 2))
 
 
-def step(world):
-    new_world = [[False] * len(world[0]) for _ in range(len(world))]
+def step(world, new_world):
+    output = ""
     for x in range(len(world)):
         for y in range(len(world[x])):
             if check_livelihood(world, x, y):
                 live(new_world, x, y)
+                output += 'O'
             else:
                 die(new_world, x, y)
-    return new_world, new_world == world
-
-
-def display(world):
+                output += ' '
+        output += '\n'
     os.system('clear')
-    for x in range(len(world)):
-        line = ""
-        for y in range(len(world[x])):
-            if alive(world, x, y):
-                line += 'O'
-            else:
-                line += ' '
-        print(line)
+    print(output)
+    return new_world == world
 
 
 def first_world(max_x=100, max_y=200, population=1000, template=""):
@@ -83,6 +76,8 @@ def first_world(max_x=100, max_y=200, population=1000, template=""):
     max_y = int(max_y)
     population = int(population)
     world = [[False] * max_y for _ in range(max_x)]
+    next_world = [[False] * max_y for _ in range(max_x)]
+    neighbourhoods = [[None] * max_y for _ in range(max_x)]
     if population > 0:
         x = random.randint(0, max_x - 1)
         y = random.randint(0, max_y - 1)
@@ -106,13 +101,15 @@ def first_world(max_x=100, max_y=200, population=1000, template=""):
                 x += 1
                 if x == max_x:
                     break
-    return world
+    return world, next_world, neighbourhoods
 
 
 if __name__ == "__main__":  # pragma: no cover
     done = False
-    current_world = first_world(*sys.argv[1:])
+    current_world, new_world, NEIGHBOURHOODS = first_world(*sys.argv[1:])
     while not done:
-        display(current_world)
-        current_world, done = step(current_world)
-    display(current_world)
+        done = step(current_world, new_world)
+        tmp = current_world
+        current_world = new_world
+        new_world = tmp
+
